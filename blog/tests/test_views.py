30ee776c330465f4381349c4b34fa17c5a7ab308
jing Wang <hi@jingwang.me>
2023-05-07 13:25:17 +0800

    now can display all posts

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -3,9 +3,26 @@ from django.urls import resolve
 from blog.views import home_page
 from django.http import HttpRequest
 from functional_tests.const import HOME_TITLE
+from django.urls import reverse
+from blog.models import Post
 
 
 class HomePageTest(TestCase):
     def test_home_page_using_template(self):
-        response = self.client.get("/")
+        response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "blog/home.html")
+
+    def create_post(self, title, content):
+        Post.objects.create(title=title, content=content)
+
+    def test_display_all_post(self):
+        first_title = "The first title"
+        first_content = "first content"
+        second_title = "The second title"
+        second_content = "second content"
+
+        self.create_post(first_title, first_content)
+        self.create_post(second_title, second_content)
+
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, first_title)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, "blog/home.html")
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
